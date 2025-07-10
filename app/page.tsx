3e38d177b5ad228fbcfce316dab056c3a351258e
Pritam Pramanik <pritam.pramanik.ece26@heritageit.edu.in>
2025-07-10 23:25:26 +0530

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,102 @@
-import Image from "next/image";
+import Marquee from './marquee';
+import Orbite from './orbiting';
+import SmoothCursorDemo from './smoothCursor';
+import DotPattern from '@/components/magicui/dot-pattern';
+import { RainbowButton } from '@/components/magicui/rainbow-button';
+import { Terminal } from '@/components/magicui/terminal';
+import { AuroraText } from "@/components/magicui/aurora-text";
+import { AnimatedList } from "@/components/magicui/animated-list";
+import { SparklesText } from "@/components/magicui/sparkles-text";
+import { AvatarCircles } from "@/components/magicui/avatar-circles";
+import { Ripple } from "@/components/magicui/ripple";
+import { MorphingText } from "@/components/magicui/morphing-text";
+import { WarpBackground } from "@/components/magicui/warp-background";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <WarpBackground className='p-2'>
+
+
+
+<div className="fixed top-0 left-1/2 transform -translate-x-1/2 flex justify-between p-2 bg-white/50 backdrop-blur-sm z-50 shadow-sm rounded-3xl w-1/2">
+           <SparklesText className='text-[35px] p-1'>Pritam.</SparklesText>
+          <div>
+            <RainbowButton className='mt-3 mr-3'>Let's gooo!💪🏻</RainbowButton>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+
+
+
+        <div className="min-h-screen flex flex-col items-center gap-4 mt-50">
+
+          <MorphingText texts={["Pritam", "Pramanik"]} />
+          <div><Terminal className='bg-gray-100 w-3xl'><pre className="text-gray-950">
+            pnpm dlx shadcn@latest init<br />
+
+            ✔ Preflight checks.<br />
+
+            ✔ Verifying framework. Found Next.js.<br />
+
+            ✔ Validating Tailwind CSS.<br />
+
+            ✔ Validating import alias.<br />
+
+            ✔ Writing components.json.<br />
+
+            ✔ Checking registry.<br />
+
+            ✔ Updating tailwind.config.ts<br />
+
+            ✔ Updating app/globals.css<br />
+
+            ✔ Installing dependencies.<br />
+
+            i Updated 1 file:<br />
+
+            - lib/utils.ts<br />
+
+            Success! Project initialization completed.<br />
+            You may now add components.</pre>
+          </Terminal></div>
+
+
+
+        </div>
+
+
+
+
+        <div>
+          <Marquee />
+        </div>
+
+        <div className="relative flex items-center justify-center mt-2">
+          {/* Ripple as background */}
+          <div className="absolute inset-0 flex items-center justify-center -z-10">
+            <Ripple />
+          </div>
+
+          {/* Orbit on top */}
+          <Orbite />
+        </div>
+      </WarpBackground>
+
+
+      {/* <div>
+        <div>
+        <DotPattern/>
+        </div>
+
+
+      </div>
+      <SmoothCursorDemo /> */}
     </div>
+
+
+
+
   );
 }
